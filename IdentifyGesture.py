155a6b46839299c0c578d719fca42c50be15be45
ayushtamra <tamraayush@gmail.com>
2022-07-19 02:14:13 +0530

    Update IdentifyGesture.py with some code comments
    Signed-off-by: ayushtamra <tamraayush@gmail.com>

--- IdentifyGesture.py
+++ IdentifyGesture.py
@@ -125,8 +125,9 @@ def overlay(image, emoji, x, y, w, h):
 
 
 def blend_transparent(face_img, overlay_t_img):
-    overlay_img = overlay_t_img[:, :, :3]
-    overlay_mask = overlay_t_img[:, :, 3:]
+    # Split out the transparency mask from the colour info
+    overlay_img = overlay_t_img[:, :, :3]  # Grab the BRG planes
+    overlay_mask = overlay_t_img[:, :, 3:]  # And the alpha plane
 
     # Calculating inverse mask again
     background_mask = 255 - overlay_mask
@@ -135,9 +136,12 @@ def blend_transparent(face_img, overlay_t_img):
     overlay_mask = cv2.cvtColor(overlay_mask, cv2.COLOR_GRAY2BGR)
     background_mask = cv2.cvtColor(background_mask, cv2.COLOR_GRAY2BGR)
 
+    # Create a masked out face image, and masked out overlay
+    # We convert the images to floating point in range 0.0 - 1.0
     face_part = (face_img * (1 / 255.0)) * (background_mask * (1 / 255.0))
     overlay_part = (overlay_img * (1 / 255.0)) * (overlay_mask * (1 / 255.0))
 
+    # And finally just add them together, and rescale it back to an 8bit integer image
     return np.uint8(cv2.addWeighted(face_part, 255.0, overlay_part, 255.0, 0.0))
 
 
